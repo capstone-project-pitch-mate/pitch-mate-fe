0e2d5f949b117abf77d44c11b180682022ad78e5
holdn2 <yoocy01@gmail.com>
2026-04-08 14:33:59 +0900

    chore: tailwind css 자동 정렬 및 포맷을 위한 prettierrc 설정

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
         <h1 className="text-3xl font-bold text-blue-600">
           React + TypeScript + Tailwind
         </h1>
-        <p className="mt-4 text-gray-600">pnpm 초기 세팅 완료</p>
+        <p className="mt-4 bg-amber-50 text-gray-600">pnpm 초기 세팅 완료</p>
       </div>
     </div>
   );
