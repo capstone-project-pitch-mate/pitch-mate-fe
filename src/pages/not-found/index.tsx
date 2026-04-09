384d172b552c96f90b38d7915595ecb008cf8066
holdn2 <yoocy01@gmail.com>
2026-04-09 13:42:24 +0900

    design: 스타일 수정

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,10 +1,10 @@
 export default function NotFound() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <div className="min-w-100 flex-col rounded-2xl border-2 border-[#6868FF] bg-[#FAFAFF] p-10 shadow-lg">
+      <div className="flex min-w-100 flex-col gap-4 rounded-2xl border-2 border-[#6868FF] bg-[#FAFAFF] p-10 shadow-lg">
         <h1 className="text-4xl font-bold text-[#6868FF]">404</h1>
-        <p className="mt-4 mb-8 text-2xl font-semibold">Page Not Found :)</p>
-        <a href="/" className="text-lg text-[#6868FF]">
+        <p className="text-2xl font-semibold">Page Not Found :)</p>
+        <a href="/" className="mt-4 text-lg text-[#6868FF]">
           홈으로 이동
         </a>
       </div>
