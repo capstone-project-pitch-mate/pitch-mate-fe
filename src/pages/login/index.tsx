3e196f42d17b67868b2fc1d0fd8b0a21f89c8f19
holdn2 <yoocy01@gmail.com>
2026-04-10 12:12:18 +0900

    feature: 로그인 회원가입 페이지 UI 구현

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,54 @@
+import { useState } from "react";
+
+import { Button, InputBar } from "@shared/ui";
+import { Link, useNavigate } from "react-router-dom";
+
 export default function Login() {
-  return <div>로그인</div>;
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // TODO: 서버 로그인 api 연동
+    navigate("/");
+  };
+
+  return (
+    <div className="flex flex-col items-center gap-10">
+      <div className="flex flex-col items-center gap-2.5">
+        <h4 className="text-2xl leading-6 font-medium text-[#1A1A2E]">
+          로그인
+        </h4>
+        <p className="text-2xl leading-9 text-[#71718A]">
+          계정에 로그인하여 개선해보세요
+        </p>
+      </div>
+      <form className="flex w-full flex-col gap-6" onSubmit={handleLogin}>
+        <InputBar
+          label="이메일"
+          text={email}
+          handleChangeText={setEmail}
+          placeholder="email@example.com"
+        />
+        <InputBar
+          label="비밀번호"
+          type="password"
+          text={password}
+          handleChangeText={setPassword}
+          placeholder="비밀번호 입력"
+        />
+        <Button size="full" color="primary" type="submit">
+          로그인
+        </Button>
+      </form>
+      <span className="text-xl leading-8 text-[#71718A]">
+        계정이 없으신가요?{" "}
+        <Link className="font-medium text-[#6868FF]" to="/signup">
+          회원가입
+        </Link>
+      </span>
+    </div>
+  );
 }
---
+++ src/shared/ui/button/index.tsx
@@ -0,0 +1,47 @@
+import { cn } from "@utils/cn";
+import type { ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode;
+  size?: "sm" | "lg" | "full";
+  color?: "primary" | "secondary";
+  type?: "submit" | "reset" | "button";
+  disabled?: boolean;
+  handleClick?: () => void;
+}
+
+export default function Button({
+  children,
+  size = "sm",
+  color = "primary",
+  type = "button",
+  disabled = false,
+  handleClick,
+}: ButtonProps) {
+  const buttonStyle =
+    size === "sm"
+      ? "p-3 w-fit"
+      : size === "lg"
+        ? "p-4.5 w-fit"
+        : "p-4.5 w-full";
+  const buttonColor =
+    color === "primary"
+      ? "bg-[#6868FF] text-white"
+      : "bg-white text-[#1A1A2E] border border-[rgba(0,0,0,0.08)]";
+
+  return (
+    <button
+      className={cn(
+        "items-center justify-center rounded-xl",
+        buttonStyle,
+        buttonColor,
+        disabled && "bg-[#ADADAD]",
+      )}
+      type={type}
+      onClick={handleClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
